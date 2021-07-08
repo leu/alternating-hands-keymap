c57eec846db65f6ac4658cfc1efac33afcc39ed8
Daniel <daniel.simols@outlook.com>
2021-07-08 22:45:13 +0100

    implemented microsolver recursively with better big O

--- src/io/github/leu/MicroSolver.java
+++ src/io/github/leu/MicroSolver.java
@@ -8,16 +8,13 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.HashSet;
 
 public class MicroSolver {
     public static JSONArray pairFrequencies;
     public static long bestScore = 0;
     public static ArrayList<Character> bestListA = new ArrayList<>();
     public static ArrayList<Character> listA = new ArrayList<>();
-    public static int[] positions = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
     public static void main(String[] args) {
         findOptimalKeymap();
@@ -25,22 +22,40 @@ public class MicroSolver {
 
     private static void findOptimalKeymap() {
         initialiseFrequencies();
-        char[] arrayA = "aeghijkoquxyz".toCharArray();
+        char[] arrayA = "aeghijkoquxyz".toCharArray(); //"aeghijkoquxyz" "bcdflmnprstvw"
         for (char letter : arrayA) {
             listA.add(letter);
         }
         for (int i = 0; i < 13; i++) {
             bestListA.add('a');
         }
+        recursiveLayoutCheck(new int[13], 0, new int[]{3, 3, 3, 4});
+    }
 
-        int[] endPositions = {12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-        while (!Arrays.equals(positions, endPositions)) {
-            //System.out.println(Arrays.toString(positions));
-            evaluateScore();
-            stepPosition(12, new PriorityQueue<>());
+    private static void recursiveLayoutCheck(int[] positions, int n, int[] groupNumAvailable) {
+        if (n < 12) {
+            for (int i = 0; i < 4; i++) {
+                if (groupNumAvailable[i] > 0) {
+                    int[] newPositions = positions.clone();
+                    newPositions[n] = i;
+                    int[] newGroupNumAvailable = groupNumAvailable.clone();
+                    newGroupNumAvailable[i]--;
+                    recursiveLayoutCheck(newPositions, n + 1, newGroupNumAvailable);
+                }
+            }
+        } else {
+            for (int i = 0; i < 4; i++) {
+                if (groupNumAvailable[i] > 0) {
+                    positions[n] = i;
+//                    StringBuilder groups = new StringBuilder();
+//                    for (int group : positions) {
+//                        groups.append(group);
+//                    }
+//                    System.out.println(groups);
+                    evaluateScore(positions);
+                }
+            }
         }
-        System.out.println(bestListA);
-        System.out.println(bestScore);
     }
 
     private static void initialiseFrequencies() {
@@ -54,17 +69,17 @@ public class MicroSolver {
         pairFrequencies = new JSONArray(content);
     }
 
-    private static void evaluateScore() {
+    private static void evaluateScore(int[] positions) {
         long score = 0L;
         for (int i = 0; i < pairFrequencies.length(); i++) {
             JSONArray pairFrequency = pairFrequencies.getJSONArray(i);
             String lettersPair = pairFrequency.getString(0);
             char letterA = lettersPair.charAt(0);
             char letterB = lettersPair.charAt(1);
-            int letterACategory = -1, letterBCategory = -1;
+            int letterACategory, letterBCategory;
             if (listA.contains(letterA) && listA.contains(letterB)) {
-                letterACategory = setLetterPositionCategory(letterA);
-                letterBCategory = setLetterPositionCategory(letterB);
+                letterACategory = setLetterPositionCategory(letterA, positions);
+                letterBCategory = setLetterPositionCategory(letterB, positions);
                 if (letterACategory != letterBCategory) {
                     score += pairFrequency.getLong(1);
                 }
@@ -72,15 +87,19 @@ public class MicroSolver {
         }
         if (score > bestScore) {
             bestScore = score;
-            for (int i = 0; i < 13; i++) {
-                bestListA.set(positions[i], listA.get(i));
+            ArrayList<HashSet<Character>> bestLayout = new ArrayList<>();
+            for (int i = 0; i < 4; i++) {
+                bestLayout.add(new HashSet<>());
             }
-            System.out.println(bestListA + "\n");
+            for (int i = 0; i < 13; i++) {
+                bestLayout.get(positions[i]).add(listA.get(i));
+            }
+            System.out.println(bestLayout);
             System.out.println(bestScore);
         }
     }
 
-    private static int setLetterPositionCategory(char letter) {
+    private static int setLetterPositionCategory(char letter, int[] positions) {
         if (positions[listA.indexOf(letter)] < 3) {
             return 1;
         } else if (positions[listA.indexOf(letter)] < 6 && positions[listA.indexOf(letter)] >= 3) {
@@ -90,28 +109,5 @@ public class MicroSolver {
         } else {
             return 4;
         }
-    }
-
-    private static void stepPosition(int i, PriorityQueue<Integer> availableNumbers) {
-        AtomicInteger firstGreaterNumber = new AtomicInteger(-1);
-        getNextGreaterPosition(availableNumbers, firstGreaterNumber, i);
-        if (firstGreaterNumber.get() == -1) {
-            availableNumbers.add(positions[i]);
-            stepPosition(i-1, availableNumbers);
-            assert(!availableNumbers.isEmpty());
-            positions[i] = availableNumbers.poll();
-        } else {
-            availableNumbers.add(positions[i]);
-            availableNumbers.remove(firstGreaterNumber.get());
-            positions[i] = firstGreaterNumber.get();
-        }
-    }
-
-    private static void getNextGreaterPosition(PriorityQueue<Integer> availableNumbers, AtomicInteger firstGreaterNumber, int i) {
-        availableNumbers.forEach( a -> {
-            if (firstGreaterNumber.get() == -1 && a > positions[i]) {
-                firstGreaterNumber.set(a);
-            }
-        } );
     }
 }
